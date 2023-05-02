616adf6368236e7f8c08af6eedb14128faafab58
João Pedro <joaopsj007@gmail.com>
2023-05-02 17:25:19 -0300

    Refatorado

--- index.js
+++ index.js
@@ -1,191 +1,177 @@
-let jogada = 0;
+let jogadas = 0;
 
-const buttonsDisabled = document.querySelector(".gameContainer").children;
-const jogadas = [];
+const combinations = () => {
+  const buttonWin = document.querySelector(".gameContainer").children;
 
-for (let i = 0; i < buttonsDisabled.length; i++) {
-  buttonsDisabled[i].disabled = true;
+  //   // verificação horizontal
+  if (
+    buttonWin[0].textContent === buttonWin[1].textContent &&
+    buttonWin[2].textContent === buttonWin[1].textContent &&
+    buttonWin[1].textContent !== ""
+  ) {
+    return true;
+  }
+
+  if (
+    buttonWin[3].textContent === buttonWin[4].textContent &&
+    buttonWin[5].textContent === buttonWin[4].textContent &&
+    buttonWin[4].textContent !== ""
+  ) {
+    return true;
+  }
+
+  if (
+    buttonWin[6].textContent === buttonWin[7].textContent &&
+    buttonWin[8].textContent === buttonWin[7].textContent &&
+    buttonWin[7].textContent !== ""
+  ) {
+    return true;
+  }
+  // verificação horizontal
+
+  // verificação vertical
+
+  if (
+    buttonWin[0].textContent === buttonWin[3].textContent &&
+    buttonWin[6].textContent === buttonWin[3].textContent &&
+    buttonWin[3].textContent !== ""
+  ) {
+    return true;
+  }
+
+  if (
+    buttonWin[1].textContent === buttonWin[4].textContent &&
+    buttonWin[7].textContent === buttonWin[4].textContent &&
+    buttonWin[4].textContent !== ""
+  ) {
+    return true;
+  }
+
+  if (
+    buttonWin[2].textContent === buttonWin[5].textContent &&
+    buttonWin[8].textContent === buttonWin[5].textContent &&
+    buttonWin[5].textContent !== ""
+  ) {
+    return true;
+  }
+  // verificação vertical
+
+  // verificação diagonal
+
+  if (
+    buttonWin[0].textContent === buttonWin[4].textContent &&
+    buttonWin[8].textContent === buttonWin[4].textContent &&
+    buttonWin[4].textContent !== ""
+  ) {
+    return true;
+  }
+
+  if (
+    buttonWin[2].textContent === buttonWin[4].textContent &&
+    buttonWin[6].textContent === buttonWin[4].textContent &&
+    buttonWin[4].textContent !== ""
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+function checkedWin(namePlayerWin) {
+  if (combinations()) {
+    Swal.fire({
+      title: namePlayerWin + " Win",
+      text: "Do you want to continue",
+      icon: "success",
+      showCancelButton: true,
+      cancelButtonText: "Exit",
+      confirmButtonText: "Play Again",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        clear();
+      } else if (!result.isConfirmed) {
+        document.getElementById("btnGo").style.display = "block";
+        const cells = document.querySelector(".gameContainer").children;
+        for (let i = 0; i < cells.length; i++) {
+          cells[i].disabled = true;
+        }
+      }
+    });
+  } else if (!combinations() && jogadas === 9) {
+    Swal.fire({
+      title: "Empate",
+      text: "Do you want to continue",
+      icon: "warning",
+      showCancelButton: true,
+      cancelButtonText: "Exit",
+      confirmButtonText: "Play Again",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        clear();
+      } else if (!result.isConfirmed) {
+        document.getElementById("btnGo").style.display = "block";
+        const cells = document.querySelector(".gameContainer").children;
+        for (let i = 0; i < cells.length; i++) {
+          cells[i].disabled = true;
+        }
+      }
+    });
+  }
 }
 
-document.getElementById("btnGo").addEventListener("click", function (ev) {
-  jogadas.length = 0;
-  document.getElementById("namePlayerWin").style.display = "none";
+let currentPlayer = "X";
 
-  const playerX = document.getElementById("namePlayerX").value;
-  const playerO = document.getElementById("namePlayerO").value;
+function moves(buttonNumber) {
+  const cell = document.querySelector(".gameContainer").children[buttonNumber];
 
-  if (!playerO || playerX === "") {
-    document.getElementById("namePlayerWin").style.display = "block";
-    document.getElementById("namePlayerWin").innerText =
-      "preencha todos os campos";
-  } else {
-    for (let i = 0; i < buttonsDisabled.length; i++) {
-      buttonsDisabled[i].disabled = false;
-      document.getElementById("btnGo").style.display = "none";
-    }
-    ("");
-  }
-});
-
-document.getElementById("playAgain").addEventListener("click", function (ev) {
-  const buttonClear = document.querySelector(".gameContainer").children;
-
-  for (let i = 0; i < buttonClear.length; i++) {
-    buttonClear[i].textContent = "";
-    buttonClear[i].classList.remove("win");
-    buttonClear[i].disabled = true;
-  }
-
-  jogada = 0;
-
-  document.getElementById("namePlayerO").value = "";
-  document.getElementById("namePlayerX").value = "";
-  document.getElementById("namePlayerWin").style.display = "none";
-
-  document.getElementById("playAgain").style.display = "none";
-  document.getElementById("playAgain").innerText = "Jogar de Novo";
-  document.getElementById("btnGo").style.display = "block";
-});
-
-function buttonclicked(buttonNumber) {
-  const button =
-    document.querySelector(".gameContainer").children[buttonNumber];
-
-  if (!button.disabled) {
-    const verificacao = jogada % 2 == 0 ? "X" : "O";
-
-    jogada += 1;
-    button.disabled = true;
-    button.textContent = verificacao;
-    jogadas.push(button);
-    console.log(jogadas);
-  }
-
-  win();
-
+  cell.innerText = currentPlayer;
   const namePlayerWin =
-    button.textContent == "X"
+    currentPlayer === "X"
       ? document.getElementById("namePlayerX").value
       : document.getElementById("namePlayerO").value;
+  jogadas.length++;
+  jogadas++;
+  checkedWin(namePlayerWin.trim());
+  currentPlayer = currentPlayer === "O" ? "X" : "O";
+  cell.disabled = true;
+}
 
-  if (win()) {
-    document.getElementById(
-      "namePlayerWin"
-    ).innerText = `${namePlayerWin} Venceu!!!`;
-    const buttonClear = document.querySelector(".gameContainer").children;
-
-    for (let i = 0; i < buttonClear.length - 1; i++) {
-      buttonClear[i].disabled = true;
-    }
-    playAgain();
-  } else if (!win() && jogadas.length === 9) {
-    document.getElementById("namePlayerWin").style.display = "block";
-    document.getElementById("namePlayerWin").innerText = `Empate`;
-    playAgain();
-  }
-
-  function playAgain() {
-    document.getElementById("namePlayerWin").style.display = "block";
-    document.getElementById("playAgain").style.display = "block";
-  }
-
-  function win() {
-    const buttonWin = document.querySelector(".gameContainer").children;
-
-    // verificação horizontal
-
-    if (
-      buttonWin[0].textContent === buttonWin[1].textContent &&
-      buttonWin[2].textContent === buttonWin[1].textContent &&
-      buttonWin[1].textContent !== ""
-    ) {
-      buttonWin[0].className = "win";
-      buttonWin[1].className = "win";
-      buttonWin[2].className = "win";
-      return true;
-    }
-
-    if (
-      buttonWin[3].textContent === buttonWin[4].textContent &&
-      buttonWin[5].textContent === buttonWin[4].textContent &&
-      buttonWin[4].textContent !== ""
-    ) {
-      buttonWin[3].className = "win";
-      buttonWin[4].className = "win";
-      buttonWin[5].className = "win";
-      return true;
-    }
-
-    if (
-      buttonWin[6].textContent === buttonWin[7].textContent &&
-      buttonWin[8].textContent === buttonWin[7].textContent &&
-      buttonWin[7].textContent !== ""
-    ) {
-      buttonWin[6].className = "win";
-      buttonWin[7].className = "win";
-      buttonWin[8].className = "win";
-      return true;
-    }
-    // verificação horizontal
-
-    // verificação vertical
-
-    if (
-      buttonWin[0].textContent === buttonWin[3].textContent &&
-      buttonWin[6].textContent === buttonWin[3].textContent &&
-      buttonWin[3].textContent !== ""
-    ) {
-      buttonWin[0].className = "win";
-      buttonWin[3].className = "win";
-      buttonWin[6].className = "win";
-      return true;
-    }
-
-    if (
-      buttonWin[1].textContent === buttonWin[4].textContent &&
-      buttonWin[7].textContent === buttonWin[4].textContent &&
-      buttonWin[4].textContent !== ""
-    ) {
-      buttonWin[1].className = "win";
-      buttonWin[4].className = "win";
-      buttonWin[7].className = "win";
-      return true;
-    }
-
-    if (
-      buttonWin[2].textContent === buttonWin[5].textContent &&
-      buttonWin[8].textContent === buttonWin[5].textContent &&
-      buttonWin[5].textContent !== ""
-    ) {
-      buttonWin[2].className = "win";
-      buttonWin[5].className = "win";
-      buttonWin[8].className = "win";
-      return true;
-    }
-    // verificação vertical
-
-    // verificação diagonal
-
-    if (
-      buttonWin[0].textContent === buttonWin[4].textContent &&
-      buttonWin[8].textContent === buttonWin[4].textContent &&
-      buttonWin[4].textContent !== ""
-    ) {
-      buttonWin[0].className = "win";
-      buttonWin[4].className = "win";
-      buttonWin[8].className = "win";
-      return true;
-    }
-
-    if (
-      buttonWin[2].textContent === buttonWin[4].textContent &&
-      buttonWin[6].textContent === buttonWin[4].textContent &&
-      buttonWin[4].textContent !== ""
-    ) {
-      buttonWin[2].className = "win";
-      buttonWin[4].className = "win";
-      buttonWin[6].className = "win";
-      return true;
-    }
+function clear() {
+  const cells = document.querySelector(".gameContainer").children;
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].disabled = false;
+    cells[i].innerText = "";
+    jogadas = 0;
+    currentPlayer = "X";
   }
 }
+
+document.getElementById("btnGo").addEventListener("click", () => {
+  const playerX = document.getElementById("namePlayerX").value.trim();
+  const playerO = document.getElementById("namePlayerO").value.trim();
+
+  if (playerX === "" || playerO === "") {
+    Swal.fire({
+      width: 200,
+      position: "top-end",
+      icon: "error",
+      title: "Preencha Todos os Campos",
+      showConfirmButton: false,
+      timer: 900,
+    });
+  } else {
+    document.getElementById("btnGo").style.display = "none";
+    clear();
+    const cells = document.querySelector(".gameContainer").children;
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].disabled = false;
+
+      cells[i].addEventListener("mouseover", (ev) => {
+        ev.target.textContent = currentPlayer;
+      });
+      cells[i].addEventListener("mouseout", (ev) => {
+        ev.target.textContent = "";
+      });
+    }
+  }
+});
